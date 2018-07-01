e2f9d59ca384d2861ffa62226919be72fa169767
petrarch1603 <petrarch1603@gmail.com>
2018-07-01 13:10:58 -0600

    solved

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
 #write your code here
+def echo(str)
+	return str
+end
+
+def shout(str)
+	return str.upcase
+end
+
+def repeat(str, iters=2)
+	return ((str + " ") * iters)[0..-2]
+end
+
+def start_of_word(str, number)
+	return str[0..(number-1)]
+end
+
+def first_word(str)
+	return str.split(" ")[0]
+end
+
+def titleize(str)
+	little_words = ['a', 'the', 'and', 'if', 'so', 'over']
+	my_title = []
+	my_split = (str.split(' '))
+	my_split.map do |n|
+		if !little_words.include? n
+			my_title.push(n.capitalize)
+		else
+			my_title.push(n)
+		end
+	end
+	my_title[0] = my_title[0].capitalize
+	return my_title.join(" ")
+end
