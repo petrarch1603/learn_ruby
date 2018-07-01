2860b93604caea46703f3f6dd5e896b888367ac0
petrarch1603 <petrarch1603@gmail.com>
2018-07-01 16:27:54 -0600

    solved

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
-# write your code here
+	def title
+		@title
+	end
+
+	def title=(title)
+		@title = titilize(title)
+	end
+
+	def titilize(title)
+		articles = ['and', 'the', 'of', 'an', 'a', 'to', 'in', 'at' ]
+		result_title = []
+		title = title.split(' ')
+		title[0] = title[0].capitalize
+		for word in title do
+			if !articles.include?(word)
+				result_title.push(word.capitalize)
+			else
+				result_title.push(word)
+			end
+		end
+		result_title.join(' ')
+	end
 end
