60ed08a0e9d871d226c5cb7499de41cc9b6a3a08
petrarch1603 <petrarch1603@gmail.com>
2018-07-01 12:07:56 -0600

    added tests solved

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
-#write your code here
+def add(a, b)
+	return a + b
+end
+
+def subtract(a, b)
+	return a - b
+end
+
+def sum(array)
+	array.reduce(0, :+)
+end
+
+def multiply(*array)
+	array.reduce(1, :*)
+end
+
+def power(a, power)
+	return a**power
+end
+
+def factorial(num)
+	result = 1
+	while num > 0
+		result = num * result
+		num = num - 1
+	end
+	return result
+end
